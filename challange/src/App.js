c2a0d04c4ce770990a6057340c57a319f3e7212c
BrunoSilvaFaria <fariabruno443@gmail.com>
2023-07-27 16:54:51 -0300

    feat: exibição de componentes

---
+++ challange/src/App.js
@@ -0,0 +1,14 @@
+import Header from './componentes/Header/Header';
+import Users from './pages/Users/Users';
+import Footer from './componentes/Footer/Footer';
+function App() {
+  return (
+    <>
+      <Header />
+      <Users />
+      <Footer />
+    </>
+  );
+}
+
+export default App;
