15fa4efb1e79a30b5b6f5bc5345d4ef37059835c
BrunoSilvaFaria <fariabruno443@gmail.com>
2023-07-28 14:33:00 -0300

    feat: adição de filtro por estado, genero e idade

--- challange/src/componentes/Card/Filter/Filter.jsx
+++ challange/src/componentes/Card/Filter/Filter.jsx
@@ -1,214 +1,73 @@
-import React from "react";
+import {React, useState, useEffect} from "react";
 import { Conteiner } from "./styles";
-import SelecionaElementos from "../../../helpers/selecionaElementos";
+import estados from "./estados";
+
 function Filter({ onFilter }) {
-  const handleCheckboxChange = (event) => {
-    const valorSelecionado = event.target.value;
-    onFilter(valorSelecionado);
+  const [selectedState, setSelectedState] = useState("all");
+  const [selectedGenre, setSelectedGenre] = useState("all");
+  const [selectedAge, setSelectedAge] = useState("all");
+
+  const handleSelectState = (event) => {
+    const selectedValue = event.target.value;
+    setSelectedState(selectedValue);
+    onFilter(selectedValue, selectedState);
   };
+
+  const handleSelectGenre = (event) => {
+    const selectedValue = event.target.value;
+    setSelectedGenre(selectedValue);
+    onFilter(selectedValue, selectedGenre);
+  };
+
+  const handleSelectAge= (event) => {
+    const selectedValue = event.target.value;
+    setSelectedAge(selectedValue);
+    onFilter(selectedValue, selectedAge);
+  };
+
+  useEffect(() => {
+    onFilter(selectedState, selectedGenre, selectedAge);
+  }, [selectedState, selectedGenre, selectedAge]);
+
+  const options = estados.map((item) => (
+    <option key={item.id} value={item.value}>
+      {item.label}
+    </option>
+  ));
+
   return (
     <Conteiner>
       <div className="align">
         <form>
-          <h3 className="title">Por Estado</h3>
-          <label className="conteiner-input">
-            <input type="radio" name="estado" id="acre" onChange={handleCheckboxChange} value="acre" />
-            Acre
-          </label>
+          <div>
+            <h3 className="title">Por Estado</h3>
+            <label htmlFor="estado">
+              <select name="estado" id="estado" onChange={handleSelectState}>
+                {options}
+              </select>
+            </label>
+          </div>
 
-          <label className="conteiner-input">
-            <input type="radio" name="estado" id="alagoas" onChange={handleCheckboxChange} value="alagoas" />
-            Alagoas
-          </label>
+          <div>
+            <h3 className="title">Por Genero</h3>
+            <label htmlFor="genre">
+              <select name="genre" id="genre" onChange={handleSelectGenre}>
+                <option value="all">Todos</option>
+                <option value="male">Masculino</option>
+                <option value="female">Ferminino</option>
+              </select>
+            </label>
+          </div>
 
-          <label className="conteiner-input">
-            <input type="radio" name="estado" id="amapá" onChange={handleCheckboxChange} value="amapá" />
-            Amapá
-          </label>
-
-          <label className="conteiner-input">
-            <input type="radio" name="estado" id="amazonas" onChange={handleCheckboxChange} value="amazonas" />
-            Amazonas
-          </label>
-
-          <label className="conteiner-input">
-            <input type="radio" name="estado" id="bahia" onChange={handleCheckboxChange} value="bahia" />
-            Bahia
-          </label>
-
-          <label className="conteiner-input">
-            <input type="radio" name="estado" id="ceará" onChange={handleCheckboxChange} value="ceará" />
-            Ceará
-          </label>
-
-          <label className="conteiner-input">
-            <input
-              type="radio"
-              name="estado"
-              id="distrito_federal"
-              onChange={handleCheckboxChange} value="distrito federal"
-            />
-            Distrito Federal
-          </label>
-
-          <label className="conteiner-input">
-            <input
-              type="radio"
-              name="estado"
-              id="espírito_santo"
-              onChange={handleCheckboxChange} value="espírito santo"
-            />
-            Espírito Santo
-          </label>
-
-          <label className="conteiner-input">
-            <input type="radio" name="estado" id="goiás" onChange={handleCheckboxChange} value="goiás" />
-            Goiás
-          </label>
-
-          <label className="conteiner-input">
-            <input type="radio" name="estado" id="maranhão" onChange={handleCheckboxChange} value="maranhão" />
-            Maranhão
-          </label>
-
-          <label className="conteiner-input">
-            <input
-              type="radio"
-              name="estado"
-              id="mato_grosso"
-              onChange={handleCheckboxChange} value="mato grosso"
-            />
-            Mato Grosso
-          </label>
-
-          <label className="conteiner-input">
-            <input
-              type="radio"
-              name="estado"
-              id="mato_grosso_do_sul"
-              onChange={handleCheckboxChange} value="mato grosso do sul"
-            />
-            Mato Grosso do Sul
-          </label>
-
-          <label className="conteiner-input">
-            <input
-              type="radio"
-              name="estado"
-              id="minas_gerais"
-              onChange={handleCheckboxChange} value="minas gerais"
-            />
-            Minas Gerais
-          </label>
-
-          <label className="conteiner-input">
-            <input type="radio" name="estado" id="pará" onChange={handleCheckboxChange} value="pará" />
-            Pará
-          </label>
-
-          <label className="conteiner-input">
-            <input type="radio" name="estado" id="paraíba" onChange={handleCheckboxChange} value="paraíba" />
-            Paraíba
-          </label>
-
-          <label className="conteiner-input">
-            <input type="radio" name="estado" id="paraná" onChange={handleCheckboxChange} value="paraná" />
-            Paraná
-          </label>
-
-          <label className="conteiner-input">
-            <input
-              type="radio"
-              name="estado"
-              id="pernambuco"
-              onChange={handleCheckboxChange} value="pernambuco"
-            />
-            Pernambuco
-          </label>
-
-          <label className="conteiner-input">
-            <input type="radio" name="estado" id="piauí" onChange={handleCheckboxChange} value="piauí" />
-            Piauí
-          </label>
-
-          <label className="conteiner-input">
-            <input
-              type="radio"
-              name="estado"
-              id="rio_de_janeiro"
-              onChange={handleCheckboxChange} value="rio de janeiro"
-            />
-            Rio de Janeiro
-          </label>
-
-          <label className="conteiner-input">
-            <input
-              type="radio"
-              name="estado"
-              id="rio_grande_do_norte"
-              onChange={handleCheckboxChange} value="rio grande do norte"
-            />
-            Rio Grande do Norte
-          </label>
-
-          <label className="conteiner-input">
-            <input
-              type="radio"
-              name="estado"
-              id="rio_grande_do_sul"
-              onChange={handleCheckboxChange} value="rio grande do sul"
-            />
-            Rio Grande do Sul
-          </label>
-
-          <label className="conteiner-input">
-            <input type="radio" name="estado" id="rondônia" onChange={handleCheckboxChange} value="rondônia" />
-            Rondônia
-          </label>
-
-          <label className="conteiner-input">
-            <input type="radio" name="estado" id="roraima" onChange={handleCheckboxChange} value="roraima" />
-            Roraima
-          </label>
-
-          <label className="conteiner-input">
-            <input
-              type="radio"
-              name="estado"
-              id="santa_catarina"
-              onChange={handleCheckboxChange} value="santa catarina"
-            />
-            Santa Catarina
-          </label>
-
-          <label className="conteiner-input">
-            <input
-              type="radio"
-              name="estado"
-              id="são_paulo"
-              onChange={handleCheckboxChange} value="são paulo"
-            />
-            São Paulo
-          </label>
-
-          <label className="conteiner-input">
-            <input type="radio" name="estado" id="sergipe" onChange={handleCheckboxChange} value="sergipe" />
-            Sergipe
-          </label>
-
-          <label className="conteiner-input">
-            <input
-              type="radio"
-              name="estado"
-              id="tocantins"
-              onChange={handleCheckboxChange} value="tocantins"
-            />
-            Tocantins
-          </label>
-          
-          <button onClick={handleCheckboxChange} value="all" type="button">
-            Ver Todos
-          </button>
+          <div>
+            <h3 className="title">Por Idade</h3>
+            <select name="age" id="age" onChange={handleSelectAge}>
+              <option value="all">Todos</option>
+              <option value="18">18-30</option>
+              <option value="30">30-50</option>
+              <option value="50">50+</option>
+            </select>
+          </div>
         </form>
       </div>
     </Conteiner>
---
+++ challange/src/componentes/Card/Filter/estados.js
@@ -0,0 +1,144 @@
+const estados = [
+  {
+    id: "all",
+    label: "Todos",
+    value: "all",
+  },
+  {
+    id: "acre",
+    label: "Acre",
+    value: "acre",
+  },
+  {
+    id: "alagoas",
+    label: "Alagoas",
+    value: "alagoas",
+  },
+  {
+    id: "amapá",
+    label: "Amapá",
+    value: "amapá",
+  },
+  {
+    id: "amazonas",
+    label: "Amazonas",
+    value: "amazonas",
+  },
+  {
+    id: "bahia",
+    label: "Bahia",
+    value: "bahia",
+  },
+  {
+    id: "ceará",
+    label: "Ceará",
+    value: "ceará",
+  },
+  {
+    id: "distrito_federal",
+    label: "Distrito Federal",
+    value: "distrito federal",
+  },
+  {
+    id: "espírito_santo",
+    label: "Espírito Santo",
+    value: "espírito santo",
+  },
+  {
+    id: "goiás",
+    label: "Goiás",
+    value: "goiás",
+  },
+  {
+    id: "maranhão",
+    label: "Maranhão",
+    value: "maranhão",
+  },
+  {
+    id: "mato_grosso",
+    label: "Mato Grosso",
+    value: "mato grosso",
+  },
+  {
+    id: "mato_grosso_do_sul",
+    label: "Mato Grosso do Sul",
+    value: "mato grosso do sul",
+  },
+  {
+    id: "minas_gerais",
+    label: "Minas Gerais",
+    value: "minas gerais",
+  },
+  {
+    id: "pará",
+    label: "Pará",
+    value: "pará",
+  },
+  {
+    id: "paraíba",
+    label: "Paraíba",
+    value: "paraíba",
+  },
+  {
+    id: "paraná",
+    label: "Paraná",
+    value: "paraná",
+  },
+  {
+    id: "pernambuco",
+    label: "Pernambuco",
+    value: "pernambuco",
+  },
+  {
+    id: "piauí",
+    label: "Piauí",
+    value: "piauí",
+  },
+  {
+    id: "rio_de_janeiro",
+    label: "Rio de Janeiro",
+    value: "rio de janeiro",
+  },
+  {
+    id: "rio_grande_do_norte",
+    label: "Rio Grande do Norte",
+    value: "rio grande do norte",
+  },
+  {
+    id: "rio_grande_do_sul",
+    label: "Rio Grande do Sul",
+    value: "rio grande do sul",
+  },
+  {
+    id: "rondônia",
+    label: "Rondônia",
+    value: "rondônia",
+  },
+  {
+    id: "roraima",
+    label: "Roraima",
+    value: "roraima",
+  },
+  {
+    id: "santa_catarina",
+    label: "Santa Catarina",
+    value: "santa catarina",
+  },
+  {
+    id: "são_paulo",
+    label: "São Paulo",
+    value: "são paulo",
+  },
+  {
+    id: "sergipe",
+    label: "Sergipe",
+    value: "sergipe",
+  },
+  {
+    id: "tocantins",
+    label: "Tocantins",
+    value: "tocantins",
+  },
+];
+
+export default estados;
